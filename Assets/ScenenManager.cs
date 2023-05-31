62d577314b7dca6d66ad4b69b5f01fe4c102c052
Anna <125206497+Naxjen@users.noreply.github.com>
2023-05-31 18:25:57 +0200

    Add files via upload
    Script zu den Menu Buttons

---
+++ Assets/ScenenManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScenenManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //OnClick Play Button
+    public void GoToGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    //OnClick Quit Button
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
